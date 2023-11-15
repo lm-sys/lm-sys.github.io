4b47c8766428bde2d57d317209899403348b9bad
shiyi.c_98 <shiyicao314@gmail.com>
2023-11-15 01:37:49 -0800

    Add S-LoRA (#39)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="/fonts/load.css" />
+        <script src="/mathjax.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.min.js" integrity="sha384-..." crossOrigin="anonymous"></script>
       </Head>
       <body>
         <Main />
